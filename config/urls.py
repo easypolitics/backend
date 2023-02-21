ac362dfc7bfa20abf62d7446027fd8b6d9bf6726
smehlhoff <stevemehlhoff@gmail.com>
2023-02-20 17:16:21 -0800

    Redirect main view to bills

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.http import JsonResponse
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("v1/", include("backend.bills.urls", namespace="bills"))
+    path("v1/", include("backend.bills.urls", namespace="bills")),
+    path("", lambda req: redirect("v1/bills"))
 ]
 
 admin.site.site_header = getattr(settings, "ADMIN_SITE_HEADER")
