b502ccca8f500d1aafa50fcacf982a5fbb6e6fdd
smehlhoff <stevemehlhoff@gmail.com>
2023-02-20 16:44:58 -0800

    Include search functionality for bill information

--- backend/bills/admin.py
+++ backend/bills/admin.py
@@ -5,6 +5,7 @@ from .models import Bills
 
 class BillsAdmin(admin.ModelAdmin):
     list_display = ("bill_id", "short_title")
+    search_fields = ["bill_id", "summary"]
 
 
 admin.site.register(Bills, BillsAdmin)
